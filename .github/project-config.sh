06af65c75efface30f6cf54684ca54a534ff583c
jukasdrj <jukasdrj@Gmail.com>
2025-10-27 12:55:02 -0500

    Delete .github directory

--- .github/project-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# GitHub Project configuration for automation scripts
-# NOTE: Update PROJECT_NUMBER after creating the GitHub Project Board
-export PROJECT_NUMBER=2  # BooksTracker Development project
-export REPO_OWNER=$(gh repo view --json owner -q .owner.login)
-export REPO_NAME=$(gh repo view --json name -q .name)
